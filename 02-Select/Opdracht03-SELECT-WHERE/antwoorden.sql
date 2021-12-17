a893a8c733c19276164f82447a91898f753bbd1b
Thomas Mooij <2021747@talnet.nl>
2021-12-17 12:41:20 +0100

    AND/OR af

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -9,11 +9,11 @@ SELECT score FROM jaar2016 WHERE country="Netherlands";
 -- Opdracht 5
 SELECT country FROM jaar2015 WHERE region="Latin America and Caribbean";
 -- Opdracht 6 
-SELECT country FROM jaar2016 WHERE rank = 25;
+SELECT country FROM jaar2016 WHERE rank = 25, 
 -- Opdracht 7 
 SELECT country FROM jaar2016 WHERE score <= 7087 ;
 -- Opdracht 8
-SELECT country,region FROM jaar2016 WHERE rank <= 5;
+SELECT country,region FROM jaar2016 WHERE rank <= 5
 -- Opdracht 9
 SELECT score FROM jaar2015 WHERE country="Ireland";
 -- Opdracht 10
