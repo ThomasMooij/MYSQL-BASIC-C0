26dba80931d45266e9bcc70717cb33be0e2bc92b
Thomas Mooij <2021747@talnet.nl>
2021-12-21 14:04:11 +0100

    function and alias af

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,19 +1,21 @@
 -- Opdracht 1 
-
+SELECT Max(wage) As hoogste_loon_speler_fc_utrecht FROM players WHERE club="FC Utrecht";
 -- Opdracht 2 
-
+ SELECT Max(wage) As gemiddeld_inkomen_voetbalspelers FROM players;
 -- Opdracht 3
-
+SELECT Sum(wage) As totaal_loon_alleSpelersGroningen FROM players WHERE club="FC Groningen";
 -- Opdracht 4
-
+SELECT Count(name) As totaal_spelers_manCity_United FROM players WHERE club= 'Manchester City' AND club='Manchester United';
 -- Opdracht 5
-
+SELECT avg(wage) As inkomen_spelers_NL FROM players WHERE nationality="Netherlands";
 -- Opdracht 6 
-
+SELECT avg (wage) As gemiddeldInkomen_onder20 FROM players WHERE age <= 20;
 -- Opdracht 7 
+SELECT avg (wage) As gemiddeldInkomen_onder20 FROM players WHERE age >= 20;
 
 -- Opdracht 8
-
+SELECT Sum(value) As value_chelsea_totaal FROM players WHERE club= "Chelsea";
 -- Opdracht 9
-
+SELECT avg(age) As gemiddelde_leeftijd FROM players;
 -- Opdracht 10
+SELECT club , wage , round(value) FROM players WHERE club="Liverpool";
